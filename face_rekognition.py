3f6ec27daaea719401fa818e2fb22eb953a984a3
itachi201 <leca_horatiu@yahoo.com>
2021-04-26 21:06:30 +0300

    FIx later

--- face_rekognition.py
+++ face_rekognition.py
@@ -1,8 +1,22 @@
 import cv2
 
-trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# Loaded some pre-trained data faces frontal profile
+trained_face_data = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# Chosed image to detect
+img = cv2.imread('easy.png')
+# Grayscaling
+gray_scaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Detect faces
+face_coordinates = trained_face_data.detectMultiScale(gray_scaled_img)
 
-img = cv2.imread('ffhq-teaser.png')
+# sprint(face_coordinates)
+
+# Draw rectangle
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x+y, w+h), (0, 0, 255), 2)
+
+cv2.imshow('Faces_detected', gray_scaled_img)
+cv2.waitKey()
 
 
 
