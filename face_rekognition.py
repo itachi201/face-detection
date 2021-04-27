797e0ca73182f1fdd0db39168d2bc9d8256237f2
itachi201 <leca_horatiu@yahoo.com>
2021-04-27 09:51:45 +0300

    face_recognition works

--- face_rekognition.py
+++ face_rekognition.py
@@ -2,20 +2,24 @@ import cv2
 
 # Loaded some pre-trained data faces frontal profile
 trained_face_data = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 # Chosed image to detect
 img = cv2.imread('easy.png')
 # Grayscaling
-gray_scaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Detect faces
-face_coordinates = trained_face_data.detectMultiScale(gray_scaled_img)
-
-# sprint(face_coordinates)
+face_coordinates = trained_face_data.detectMultiScale(gray, 1.3, 4)
 
 # Draw rectangle
 for (x, y, w, h) in face_coordinates:
-    cv2.rectangle(img, (x, y), (x+y, w+h), (0, 0, 255), 2)
+    img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    roi_gray = gray[y:y + h, x:x + w]
+    roi_color = img[y:y + h, x:x + w]
+   # eyes = eye_cascade.detectMultiScale(roi_gray)
+   # for (ex, ey, ew, eh) in eyes:
+     #   cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
-cv2.imshow('Faces_detected', gray_scaled_img)
+cv2.imshow('Faces_detected', img)
 cv2.waitKey()
 
 
